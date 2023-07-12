5238fdc1160ed6f01618feab5ca7646ca8a9792c
WONILLISM <wonillism@gmail.com>
2023-07-12 16:33:22 +0900

    feat: add react markdown (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+import Issues from './pages/Issues';
 
 function App() {
-  const getData = async () => {
-    const response = await axios.get(
-      `${process.env.REACT_APP_GITHUB_API_URL}/repos/facebook/react/issues`,
-      {
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-          'X-GitHub-Api-Version': '2022-11-28',
-        },
-      },
-    );
-
-    console.log(response);
-  };
-
-  useEffect(() => {
-    getData();
-  });
-  return <></>;
+  return (
+    <>
+      <Issues />
+    </>
+  );
 }
 
 export default App;
